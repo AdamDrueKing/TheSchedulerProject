010e696aec0b07d6bf3f91b8930e8ef83f79c5aa
Adam King <adam81king@gmail.com>
2017-08-28 11:28:47 -0400

    initial commit

---
+++ TheSchedulerProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheSchedulerProject.Startup))]
+namespace TheSchedulerProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

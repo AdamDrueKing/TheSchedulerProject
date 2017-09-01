dfff9bbc4f7cd9515614ed339f5f1e2d22c3baf5
mjthomas22 <mthomas22@gmail.com>
2017-09-01 10:15:10 -0400

    working on API

--- TheSchedulerProject/fullcalendar-3.4.0/GCal2.js
+++ TheSchedulerProject/fullcalendar-3.4.0/GCal2.js
@@ -1,67 +1,65 @@
-﻿var CLIENT_ID = '564506460648-knam6scga0e8i8ohtfs0246nnl4ffu39.apps.googleusercontent.com';
+﻿//var CLIENT_ID = '564506460648-knam6scga0e8i8ohtfs0246nnl4ffu39.apps.googleusercontent.com';
 
 // Array of API discovery doc URLs for APIs used by the quickstart
-var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
+//var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
 
 // Authorization scopes required by the API; multiple scopes can be
 // included, separated by spaces.
-var SCOPES = "https://www.googleapis.com/auth/calendar";
+//var SCOPES = "https://www.googleapis.com/auth/calendar";
 
-var authorizeButton = document.getElementById('authorize-button');
-var signoutButton = document.getElementById('signout-button');
+//var authorizeButton = document.getElementById('authorize-button');
+//var signoutButton = document.getElementById('signout-button');
 
-/**
- *  On load, called to load the auth2 library and API client library.
- */
 function handleClientLoad() {
+    // Loads the client library and the auth2 library together for efficiency.
+    // Loading the auth2 library is optional here since `gapi.client.init` function will load
+    // it if not already loaded. Loading it upfront can save one network request.
     gapi.load('client:auth2', initClient);
 }
 
-/**
- *  Initializes the API client library and sets up sign-in state
- *  listeners.
- */
 function initClient() {
+    // Initialize the client with API key and People API, and initialize OAuth with an
+    // OAuth 2.0 client ID and scopes (space delimited string) to request access.
     gapi.client.init({
-        discoveryDocs: DISCOVERY_DOCS,
-        clientId: CLIENT_ID,
-        scope: SCOPES
+        apiKey: 'AIzaSyAsOIuikEWJisvhZ-XctdMiikl0v2LUUfw',
+        discoveryDocs: ["https://people.googleapis.com/$discovery/rest?version=v1"],
+        clientId: '564506460648-knam6scga0e8i8ohtfs0246nnl4ffu39.apps.googleusercontent.com',
+        scope: 'https://www.googleapis.com/auth/calendar'
     }).then(function () {
         // Listen for sign-in state changes.
         gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
 
         // Handle the initial sign-in state.
         updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-        authorizeButton.onclick = handleAuthClick;
-        signoutButton.onclick = handleSignoutClick;
     });
 }
 
-/**
- *  Called when the signed in status changes, to update the UI
- *  appropriately. After a sign-in, the API is called.
- */
 function updateSigninStatus(isSignedIn) {
+    // When signin status changes, this function is called.
+    // If the signin status is changed to signedIn, we make an API call.
     if (isSignedIn) {
-        authorizeButton.style.display = 'none';
-        signoutButton.style.display = 'block';
-    } else {
-        authorizeButton.style.display = 'block';
-        signoutButton.style.display = 'none';
+        makeApiCall();
     }
 }
 
-/**
- *  Sign in the user upon button click.
- */
-function handleAuthClick(event) {
+function handleSignInClick(event) {
+    // Ideally the button should only show up after gapi.client.init finishes, so that this
+    // handler won't be called before OAuth is initialized.
     gapi.auth2.getAuthInstance().signIn();
 }
 
-/**
- *  Sign out the user upon button click.
- */
-function handleSignoutClick(event) {
+function handleSignOutClick(event) {
     gapi.auth2.getAuthInstance().signOut();
 }
 
+function makeApiCall() {
+    // Make an API call to the People API, and print the user's given name.
+    gapi.client.people.people.get({
+        'resourceName': 'people/me',
+        'requestMask.includeField': 'person.names'
+    }).then(function (response) {
+        console.log('Hello, ' + response.result.names[0].givenName);
+    }, function (reason) {
+        console.log('Error: ' + reason.result.error.message);
+    });
+}
